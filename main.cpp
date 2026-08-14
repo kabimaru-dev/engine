f70a710763fd66fd51183cce1cc586f0c4ec697b
kabimaru-dev <viacheslavchubenko@animesakura.net>
2026-08-14 08:55:48 +0000

    Final: Window surface - probably not work. Never test it. Add: 'VkDeviceCreateInfo' for 'vkGetDeviceQueue'

--- main.cpp
+++ main.cpp
@@ -232,11 +232,16 @@ private:
             queueCreateInfo.queueCount = 1;
             queueCreateInfo.pQueuePriorities = &queuePriority;
             queueCreateInfos.push_back(queueCreateInfo);
+            
+            VkDeviceCreateInfo createInfo{};
 
-            // VkDeviceCreateInfo deviceInfo{};
-            // createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
-            // createInfo.pQueueCreateInfos = queueCreateInfos.data();
-            // vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
+            if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create logical device!");
+            }
+            
+            createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+            createInfo.pQueueCreateInfos = queueCreateInfos.data();
+            vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);  
         }
 
         std::cout << "5. createLogicalDevice: only QueueFamilyIndices is run" << std::endl;
